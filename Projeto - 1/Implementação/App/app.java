0ef7af8d83efa09afd2289c2508aa8ae85402b85
Matheus Brandão Freire <matheusbrandaofreire@MacBook-Air-de-Matheus.local>
2022-09-04 15:15:18 -0300

    Updating class "app.java"

--- Projeto - 1/Implementação/App/app.java
+++ Projeto - 1/Implementação/App/app.java
@@ -3,29 +3,56 @@ package app;
 import java.util.*;
 
 public class app {
-     public static void main(String args[]){
-	     Disciplina dis = new Disciplina("Disciplina", true);
-	     System.out.println(dis);
-	     Secretaria s = new Secretaria();
-	     Curso c0 = new Curso("matematica", "1");
-	     Curso c1 = new Curso("matematica", "2");
-	     Curso c2 = new Curso("matematica", "3");
-	     
-	     s.cadastrarCurso(c0);
-	     s.cadastrarCurso(c1);
-	     s.cadastrarCurso(c2);
-	     List<Curso> cursos = s.getCursos();
-	     
-	     for(Curso x : cursos) {
-	    	 System.out.println(x.getNome() + " " + x.getId());
-	     }
-	     
-	     s.deletarCurso(c1);
-	     
-	     System.out.println("\n");
-	     
-	     for(Curso x : cursos) {
-	    	 System.out.println(x.getNome() + " " + x.getId());
-	     }
-     }
+	public static void limparTela() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+	
+	public static void printMenu() {
+        limparTela();
+        System.out.println("1 - Cadastrar Usuário");
+        System.out.println("2 - Login de Usuário");
+        System.out.println("3 - Universidade - Gerar Currículo");
+        System.out.println("4 - Aluno - Cadastrar Matrícula");
+        System.out.println("4 - Professor - Listar Alunos");
+        System.out.println("0 - sair e salvar");
+    }
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        try {
+            int k;
+            do {
+                printMenu();
+                k = ler.nextInt();
+
+                switch (k) {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    default:
+                        break;
+
+                }
+                System.out.println("Press Enter to continue...");
+                System.in.read();
+            } while (k != 0);
+        } catch (Exception e) {
+            System.out.println("");
+        }
+
+
+    }
 }

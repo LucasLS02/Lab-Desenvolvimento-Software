3a0ace365c60b1472e14ee92c37c268f638367bf
Matheus Brandão Freire <80472461+Matheusbfreire@users.noreply.github.com>
2022-09-04 23:50:37 -0300

    Deleting

--- Projeto - 1/Implementação/Excecoes/ExcecaoCursoJaCadastrado.java
+++
@@ -1,9 +0,0 @@
-package Excecoes;
-
-public class ExcecaoCursoJaCadastrado extends Exception{
-    private static final long serialVersionUID = 1L;
-
-	public ExcecaoCursoJaCadastrado(){
-        super("Não é possível adicionar um curso pois o aluno já está matriculado em um curso.");
-    }
-}
--- Projeto - 1/Implementação/Excecoes/ExcecaoDisciplinaJaExistente.java
+++
@@ -1,9 +0,0 @@
-package Excecoes;
-
-public class ExcecaoDisciplinaJaExistente extends Exception{
-    private static final long serialVersionUID = 1L;
-
-	public ExcecaoDisciplinaJaExistente(){
-        super("Não é possíve adicionar essa disciplina pois ela já existe.");
-    }
-}
